1c5608a3504341727eb2793b5c6c2f5142704964
Brian Yingling <brian_yingling@yahoo.com>
2013-05-01 20:54:33 -0400

    adding gemfile

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+ruby "1.9.3"
+gem 'pg'
+gem 'sinatra'
+gem 'sinatra-reloader'
+gem 'activesupport'
+# gem 'JSON'
+gem 'httparty'
--- main.rb
+++ main.rb
@@ -10,44 +10,45 @@ require 'pg'
 # get '/' do
 #   erb :movie_data
 # end
+class App < Sinatra::Base
 
-
-get '/' do
-  if params['search'].present?
-    @search = params['search'].gsub(' ','+')
-    @data = view_movie(@search)
-    sql = "insert into movies (title, poster, year, rated, released, runtime, genre, director, writers, actors, plot) values (\'#{@data['Title']}\',\'#{@data['Poster']}\',\'#{@data['Year']}\',\'#{@data['Rated']}\',\'#{@data['Released']}\',\'#{@data['Runtime']}\',\'#{@data['Genre']}\',\'#{@data['Director']}\',\'#{@data['Writer']}\',\'#{@data['Actors']}\',\'#{@data['Plot'].gsub("'","")}\')"
-    conn = PG.connect(:dbname=> 'movie_app', :host=>'localhost')
-    conn.exec(sql)
-    conn.close
+  get '/' do
+    if params['search'].present?
+      @search = params['search'].gsub(' ','+')
+      @data = view_movie(@search)
+      sql = "insert into movies (title, poster, year, rated, released, runtime, genre, director, writers, actors, plot) values (\'#{@data['Title']}\',\'#{@data['Poster']}\',\'#{@data['Year']}\',\'#{@data['Rated']}\',\'#{@data['Released']}\',\'#{@data['Runtime']}\',\'#{@data['Genre']}\',\'#{@data['Director']}\',\'#{@data['Writer']}\',\'#{@data['Actors']}\',\'#{@data['Plot'].gsub("'","")}\')"
+      conn = PG.connect(:dbname=> 'movie_app', :host=>'localhost')
+      conn.exec(sql)
+      conn.close
+    end
+    erb :movie_data
   end
-  erb :movie_data
-end
 
-get '/faq' do
-  erb :faq
-end
+  get '/faq' do
+    erb :faq
+  end
 
-get '/about' do
-  erb :about
-end
+  get '/about' do
+    erb :about
+  end
 
-get '/movies' do
-  sql = "select poster from movies"
-  conn = PG.connect(:dbname=>'movie_app',:host=>'localhost')
-  binding.pry
-  @rows = conn.exec(sql)
-  conn.close
+  get '/movies' do
+    sql = "select poster from movies"
+    conn = PG.connect(:dbname=>'movie_app',:host=>'localhost')
+    binding.pry
+    @rows = conn.exec(sql)
+    conn.close
 
- erb :posters
-end
+   erb :posters
+  end
 
 
 
-def view_movie(search)
-  begin
-    JSON(HTTParty.get("http://www.omdbapi.com/?t=#{search}").body)
-  rescue
-    "error: could not connect"
+  def view_movie(search)
+    begin
+      JSON(HTTParty.get("http://www.omdbapi.com/?t=#{search}").body)
+    rescue
+      "error: could not connect"
+    end
   end
 end
